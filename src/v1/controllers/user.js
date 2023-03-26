107b05564049ed7d08e8f39579aed276b1e5f996
azuma-ya <kazu.bluemountain@gmail.com>
2023-03-26 17:23:15 +0900

    JWT

--- src/v1/controllers/user.js
+++ src/v1/controllers/user.js
@@ -4,7 +4,6 @@ const User = require("../models/user");
 
 exports.register = async (req, res) => {
 	const password = req.body.password;
-
 	try {
 		//パスワードの暗号化
 		req.body.password = CryptoJS.AES.encrypt(
@@ -20,5 +19,43 @@ exports.register = async (req, res) => {
 		res.status(200).json({ user, token });
 	} catch (err) {
 		res.status(500).json(err);
+	}
+};
+
+exports.login = async (req, res) => {
+	const { username, password } = req.body;
+
+	try {
+		//DBからユーザーをとってくる
+		const user = await User.findOne({ username: username });
+		if (!user) {
+			return res.status(401).json({
+				errors: {
+					param: "username",
+					message: "ユーザー名が無効です",
+				},
+			});
+		}
+		//パスワードの照合
+		const descryptedPassword = CryptoJS.AES.decrypt(
+			user.password,
+			process.env.SECRET_KEY
+		).toString(CryptoJS.enc.Utf8);
+		if (descryptedPassword !== password) {
+			return res.status(401).json({
+				errors: {
+					param: "password",
+					message: "パスワードが無効です",
+				},
+			});
+		}
+		//JWTの発行
+		const token = JWT.sign({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
+			expiresIn: "24h",
+		});
+
+		return res.status(201).json({ user, token });
+	} catch (err) {
+		return res.status(500).json(err);
 	}
 };
--- src/v1/routes/auth.js
+++ src/v1/routes/auth.js
@@ -3,6 +3,7 @@ const User = require("../models/user");
 const { body } = require("express-validator");
 const validation = require("../handlers/validation");
 const userController = require("../controllers/user");
+const tokenHandler = require("../handlers/tokenHandler");
 require("dotenv").config();
 
 //ユーザー新規登録API
@@ -29,3 +30,22 @@ router.post(
 );
 
 module.exports = router;
+
+//ログイン用API
+router.post(
+	"/login",
+	body
+		.apply("username")
+		.isLength({ min: 8 })
+		.withMessage("ユーザー名は8文字以上である必要があります。"),
+	body("password")
+		.isLength({ min: 8 })
+		.withMessage("パスワードは8文字以上である必要があります。"),
+	validation.validate,
+	userController.login
+);
+
+//JWT認証API
+router.post("/verify-token", tokenHandler.verifyToken, (req, res) => {
+	return res.status(200).json({ user: req.user });
+});
